66ad64e41379fea14f2a5e731ede3a64721b1ac9
kalitva <33896868+pavave@users.noreply.github.com>
2025-10-04 07:55:35 +0300

    Update minikit.config.ts

--- minikit.config.ts
+++ minikit.config.ts
@@ -3,10 +3,10 @@ import { defineConfig } from '@farcaster/minikit';
 export default defineConfig({
   id: 'token-info',
   name: 'Token Info',
-  description: '📈 Review of token prices, charts shared with Farcaster',
+  description: 'Review of token prices, charts shared with Farcaster',
   icon: '/icon.png',
   screenshot: '/screenshot.png',
-  developer: 'pavavalera',
+  developer: 'pavavе',
   website: 'https://token-info-miniapp.vercel.app',
   theme: {
     primary: '#1e3a8a',
